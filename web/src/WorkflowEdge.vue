4c76708d400550a91992343d2081a9635028a2a4
nya-a-cat <188900516+nya-a-cat@users.noreply.github.com>
2026-09-08 19:46:39 +0800

    Preserve outlined connection widths while zooming

--- web/src/WorkflowEdge.vue
+++ web/src/WorkflowEdge.vue
@@ -7,13 +7,14 @@ const props = defineProps({
   id: String, sourceX: Number, sourceY: Number, targetX: Number, targetY: Number,
   sourcePosition: String, targetPosition: String, markerStart: String, markerEnd: String,
   selected: Boolean, data: Object, preview: Boolean,
+  zoom: { type: Number, default: 1 },
 })
 const path = computed(() => getBezierPath(props)[0])
 const color = computed(() => props.preview ? '#ed97cd' : props.selected ? '#f4a3d3' : props.data?.layer === 'action' ? '#cf559e' : '#a6a292')
 </script>
 
 <template>
-  <path class="wire-outline" :d="path" />
+  <path class="wire-outline" :d="path" :style="{ strokeWidth: 6 / zoom }" />
   <BaseEdge :id="id" :path="path" :marker-start="markerStart" :marker-end="markerEnd"
-    :interaction-width="preview ? 0 : 24" class="wire-core" :style="{ stroke: color }" />
+    :interaction-width="preview ? 0 : 24" class="wire-core" :style="{ stroke: color, strokeWidth: 4 / zoom }" />
 </template>
